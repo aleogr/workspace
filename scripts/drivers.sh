99f0b9ad212e91b213e02aceb0bb9d8b431a9090
Alexandre Ogrodovski <2933195+aleogr@users.noreply.github.com>
2025-11-17 02:13:14 -0300

    Simplify NVIDIA GPU setup script
    Removed old steps for NVIDIA GPU setup and added installation of nvidia-kernel-dkms.

---
+++ scripts/drivers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt install -y \
+  nvidia-kernel-dkms
+
+cat << EOF > /etc/modprobe.d/99-blacklist-nvidia-vfio.conf
+blacklist nvidia
+blacklist nouveau
+EOF
+
+update-initramfs -u
+
+systemctl restart libvirtd.service
