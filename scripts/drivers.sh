4b5d8c562ea813697ae91d5e04225f812f4be119
Alexandre Ogrodovski <2933195+aleogr@users.noreply.github.com>
2025-11-18 01:04:19 -0300

    Comment out vfio module loading in drivers.sh
    Comment out lines for vfio module loading.

--- scripts/drivers.sh
+++ scripts/drivers.sh
@@ -9,6 +9,11 @@ blacklist nvidia
 blacklist nouveau
 EOF
 
+#echo "vfio" >> /etc/initramfs-tools/modules
+#echo "vfio_iommu_type1" >> /etc/initramfs-tools/modules
+#echo "vfio_pci" >> /etc/initramfs-tools/modules
+#echo "vfio_virqfd" >> /etc/initramfs-tools/modules
+
 update-initramfs -u
 
 systemctl restart libvirtd.service
